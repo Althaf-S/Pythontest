5de1231c27c151e5f1ff5b92ba86f6964ecdf6f3
Althaf S <althaf.sabsara@gmail.com>
2023-10-30 10:25:45 +0530

    Added python ex13.py

---
+++ ex13.py
@@ -0,0 +1,7 @@
+from sys import argv
+x,y,z,w = argv
+
+print("The script name is ", x) 
+print("Thr first argument is ", y)
+print("THe seconfd argument is ", z)
+print ("The third argument is ", w) 
