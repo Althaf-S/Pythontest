e0b6c8b1ffc8735ba3b9db253583879f5d800766
Althaf S <althaf.sabsara@gmail.com>
2023-10-30 09:55:46 +0530

    Added ex11.py

---
+++ ex11.py
@@ -0,0 +1,7 @@
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you (in cm's)?", end=' ')
+height = input()
+print("How much do you weigh (in Kg)?", end=' ')
+weight = input()
+print(f"So, you're {age} years old, {height}cm tall and {weight}Kg heavy.")
